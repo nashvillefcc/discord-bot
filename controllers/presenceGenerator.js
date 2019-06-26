cc3974080c02cfca0025291b78273f5e37a2f7d7
Warpfox <tylermwatts@gmail.com>
2019-06-25 21:30:30 -0500

    actually fixes export

--- controllers/presenceGenerator.js
+++ controllers/presenceGenerator.js
@@ -1,7 +1,5 @@
 const presences = require('../data/presences');
 
-export default {
-  function() {
-    return presences[Math.floor(Math.random() * presences.length)];
-  }
+module.exports = function() {
+  return presences[Math.floor(Math.random() * presences.length)];
 };
