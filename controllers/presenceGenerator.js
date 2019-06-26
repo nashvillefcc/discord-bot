456fb94d25cb811b8bc6e421bceafc4fd54b3736
Warpfox <tylermwatts@gmail.com>
2019-06-25 21:29:50 -0500

    fix export

--- controllers/presenceGenerator.js
+++ controllers/presenceGenerator.js
@@ -1,5 +1,7 @@
 const presences = require('../data/presences');
 
-export default function() {
-  return presences[Math.floor(Math.random() * presences.length)];
-}
+export default {
+  function() {
+    return presences[Math.floor(Math.random() * presences.length)];
+  }
+};
