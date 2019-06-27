d37f357a44f11fc45c7b9e7366b8841d3e0bdb3d
warpfox <tylermwatts@gmail.com>
2019-06-27 12:13:37 -0500

    updates date formatting and adds to titles

--- services/eventFetcher.js
+++ services/eventFetcher.js
@@ -2,66 +2,75 @@ const { RichEmbed } = require('discord.js');
 const fetch = require('node-fetch');
 
 module.exports = {
-  async todayEventFetcher(bot) {
-    const today = new Date(
-      new Date().toLocaleString('en-US', {
-        timeZone: 'America/Chicago'
-      })
-    );
-    const todaysDate = new Date(
-      today.getTime() - today.getTimezoneOffset() * 60000
-    )
-      .toISOString()
-      .split('T')[0];
-    const nextEvent = await fetch(
-      `https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`
-    )
-      .then(response => response.json())
-      .then(body => body[0]);
-    if (nextEvent.local_date === todaysDate) {
-      switch (nextEvent.name) {
-        case 'Mentor Night':
-          const mnEmbed = new RichEmbed()
-            .setTitle(`Mentor Night ${todaysDate}`)
-            .setURL(nextEvent.link)
-            .setThumbnail(
-              'https://secure.meetupstatic.com/photos/event/1/8/d/c/600_459726364.jpeg'
-            )
-            .setDescription(
-              nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
-                '... (Click title link for full description)'
-            );
-          bot.channels.get('586211310434254848').send(mnEmbed);
-          break;
-        default:
-          const defaultEmbed = new RichEmbed()
-            .setTitle(`${nextEvent.title} ${todaysDate}`)
-            .setURL(nextEvent.link)
-            .setThumbnail('https://i.imgur.com/pERFswi.png')
-            .setDescription(
-              nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
-                '... (Click title link for full description)'
-            );
-          bot.channels.get('586213122008809487').send(defaultEmbed);
-          break;
-      }
-    }
-  },
-  async nextEventFetcher(message) {
-    const channel = message.channel;
-    const nextEvent = await fetch(
-      `https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`
-    )
-      .then(response => response.json())
-      .then(body => body[0]);
-    const embed = new RichEmbed()
-      .setTitle(nextEvent.name)
-      .setURL(nextEvent.link)
-      .setThumbnail('https://i.imgur.com/pERFswi.png')
-      .setDescription(
-        nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
-          '... (Click title link for full description)'
-      );
-    channel.send(embed);
-  }
+	async todayEventFetcher(bot) {
+		const today = new Date(
+			new Date().toLocaleString('en-US', {
+				timeZone: 'America/Chicago',
+			})
+		);
+		const todaysDate = new Date(
+			today.getTime() - today.getTimezoneOffset() * 60000
+		)
+			.toISOString()
+			.split('T')[0];
+		const nextEvent = await fetch(
+			`https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`
+		)
+			.then(response => response.json())
+			.then(body => body[0]);
+		if (nextEvent.local_date === todaysDate) {
+			switch (nextEvent.name) {
+				case 'Mentor Night':
+					const mnEmbed = new RichEmbed()
+						.setTitle(
+							`Mentor Night ${today
+								.toLocaleString('en-US', { timeZone: 'America/Chicago' })
+								.slice(0, 9)}`
+						)
+						.setURL(nextEvent.link)
+						.setThumbnail(
+							'https://secure.meetupstatic.com/photos/event/1/8/d/c/600_459726364.jpeg'
+						)
+						.setDescription(
+							nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
+								'... (Click title link for full description)'
+						);
+					bot.channels.get('586211310434254848').send(mnEmbed);
+					break;
+				default:
+					const defaultEmbed = new RichEmbed()
+						.setTitle(`${nextEvent.title} ${todaysDate}`)
+						.setURL(nextEvent.link)
+						.setThumbnail('https://i.imgur.com/pERFswi.png')
+						.setDescription(
+							nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
+								'... (Click title link for full description)'
+						);
+					bot.channels.get('586213122008809487').send(defaultEmbed);
+					break;
+			}
+		}
+	},
+	async nextEventFetcher(message) {
+		const today = new Date()
+			.toLocaleString('en-US', {
+				timeZone: 'America/Chicago',
+			})
+			.slice(0, 9);
+		const channel = message.channel;
+		const nextEvent = await fetch(
+			`https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`
+		)
+			.then(response => response.json())
+			.then(body => body[0]);
+		const embed = new RichEmbed()
+			.setTitle(`${today} ${nextEvent.name}`)
+			.setURL(nextEvent.link)
+			.setThumbnail('https://i.imgur.com/pERFswi.png')
+			.setDescription(
+				nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
+					'... (Click title link for full description)'
+			);
+		channel.send(embed);
+	},
 };
