994110515d8e17969f6dd4e59aa649c3f3cf51d2
Warpfox <tylermwatts@gmail.com>
2019-06-28 16:46:13 -0500

    more refactoring into helper functions

---
+++ helpers/embedCreator.js
@@ -0,0 +1,14 @@
+const { RichEmbed } = require('discord.js');
+const thumbnailURL = 'https://i.imgur.com/pERFswi.png';
+const dateStringFormatter = require('./dateStringFormatter');
+
+module.exports = function(nextEvent, date) {
+  return new RichEmbed()
+    .setTitle(`${nextEvent.name} ${dateStringFormatter(date)}`)
+    .setURL(nextEvent.link)
+    .setThumbnail(thumbnailURL)
+    .setDescription(
+      nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
+        '... (Click title link for full description)'
+    );
+};
--- services/eventFetcher.js
+++ services/eventFetcher.js
@@ -1,42 +1,25 @@
 const { RichEmbed } = require('discord.js');
 const fetch = require('node-fetch');
-const dateStringFormatter = require('../helpers/dateStringFormatter');
 const todaysDateCreator = require('../helpers/todaysDateCreator');
+const embedCreator = require('../helpers/embedCreator');
 const channelIds = require('../data/channel_ids');
+const eventURL = `https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`;
 
 module.exports = {
   async todayEventFetcher(bot) {
     const todaysDate = todaysDateCreator();
-    const nextEvent = await fetch(
-      `https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`
-    )
+    const nextEvent = await fetch(eventURL)
       .then(response => response.json())
       .then(body => body[0]);
     if (nextEvent.local_date === todaysDate) {
       const date = new Date(`${nextEvent.local_date} ${nextEvent.local_time}`);
       switch (nextEvent.name) {
         case 'Mentor Night':
-          const mnEmbed = new RichEmbed()
-            .setTitle(`Mentor Night ${dateStringFormatter(date)}`)
-            .setURL(nextEvent.link)
-            .setThumbnail(
-              'https://secure.meetupstatic.com/photos/event/1/8/d/c/600_459726364.jpeg'
-            )
-            .setDescription(
-              nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
-                '... (Click title link for full description)'
-            );
+          const mnEmbed = embedCreator(nextEvent, date);
           bot.channels.get(channelIds.mentorNight_announcements).send(mnEmbed);
           break;
         default:
-          const defaultEmbed = new RichEmbed()
-            .setTitle(`${nextEvent.title} ${dateStringFormatter(date)}`)
-            .setURL(nextEvent.link)
-            .setThumbnail('https://i.imgur.com/pERFswi.png')
-            .setDescription(
-              nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
-                '... (Click title link for full description)'
-            );
+          const defaultEmbed = embedCreator(nextEvent, date);
           bot.channels
             .get(channelIds.monthlyMeetup_announcements)
             .send(defaultEmbed);
@@ -46,20 +29,11 @@ module.exports = {
   },
   async nextEventFetcher(message) {
     const channel = message.channel;
-    const nextEvent = await fetch(
-      `https://api.meetup.com/freeCodeCamp-Nashville/events?&sign=true&photo-host=public&page=1`
-    )
+    const nextEvent = await fetch(eventURL)
       .then(response => response.json())
       .then(body => body[0]);
     const date = new Date(`${nextEvent.local_date} ${nextEvent.local_time}`);
-    const embed = new RichEmbed()
-      .setTitle(`${nextEvent.name} ${dateStringFormatter(date)}`)
-      .setURL(nextEvent.link)
-      .setThumbnail('https://i.imgur.com/pERFswi.png')
-      .setDescription(
-        nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
-          '... (Click title link for full description)'
-      );
+    const embed = embedCreator(nextEvent, date);
     channel.send(embed);
   }
 };
