e2fde0b13443baee9c28feff8b686e390924bc2f
Warpfox <tylermwatts@gmail.com>
2019-06-29 13:02:49 -0500

    message options test

--- services/eventFetcher.js
+++ services/eventFetcher.js
@@ -21,7 +21,7 @@ module.exports = {
           const defaultEmbed = embedCreator(nextEvent, date);
           bot.channels
             .get(channelIds.monthlyMeetup_announcements)
-            .send(defaultEmbed);
+            .send('<@213089677652131841>', { embed: defaultEmbed });
           break;
       }
     }
