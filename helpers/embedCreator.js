c6154977240b217087f062d537a9c888149579f0
Warpfox <tylermwatts@gmail.com>
2019-06-30 17:57:15 -0500

    colored embeds?

--- helpers/embedCreator.js
+++ helpers/embedCreator.js
@@ -4,12 +4,12 @@ const dateStringFormatter = require('./dateStringFormatter');
 
 module.exports = function(nextEvent, date) {
   return new RichEmbed()
+    .setColor(0xebeef1)
     .setTitle(`${nextEvent.name} ${dateStringFormatter(date)}`)
     .setURL(nextEvent.link)
     .setThumbnail(thumbnailURL)
     .setDescription(
       nextEvent.description.replace(/<[^>]*>?/gm, '').slice(0, 280) +
         '... (Click title link for full description)'
-    )
-    .addField('Attention', '@everyone');
+    );
 };
