cd2d9654e45d9d0408f07c796fd77fe9e9353716
warpfox <tylermwatts@gmail.com>
2019-07-05 13:07:47 -0500

    date formatting

--- helpers/eventMessageCreator.js
+++ helpers/eventMessageCreator.js
@@ -1,15 +1,14 @@
 module.exports = event => {
-  return `@everyone\n**Event:**\n${event.name}\n\n**When:**\n${new Date(
-    `${event.local_date} ${event.local_time}`
-  ).toLocaleString('en-US', {
-    timeZone: 'America/Chicago',
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: '2-digit'
-  })}\n\n**Where:**\n${event.venue.name}\n${event.venue.address_1}\n${
-    event.venue.city
-  }, ${event.venue.state} ${event.venue.zip}\n\n${event.link}`;
+	return `@everyone\n**Event:**\n${event.name}\n\n**When:**\n${new Date(
+		`${event.local_date} ${event.local_time}`
+	).toLocaleString('en-US', {
+		weekday: 'long',
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric',
+		hour: 'numeric',
+		minute: '2-digit',
+	})}\n\n**Where:**\n${event.venue.name}\n${event.venue.address_1}\n${
+		event.venue.city
+	}, ${event.venue.state} ${event.venue.zip}\n\n${event.link}`;
 };
