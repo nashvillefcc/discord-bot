2e499e72b0036af6dddeff5ef3db04bd009e7858
Warpfox <tylermwatts@gmail.com>
2019-07-07 12:39:37 -0500

    remove console.log

--- services/meetupTokenRefresher.js
+++ services/meetupTokenRefresher.js
@@ -7,10 +7,9 @@ module.exports = async (id, secret, refreshToken) => {
     `https://secure.meetup.com/oauth2/access?client_id=${id}&client_secret=${secret}&grant_type=refresh_token&refresh_token=${refreshToken}`,
     {
       method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     }
   )
     .then(response => response.json())
-    .then(body => console.log(body))
     .catch(err => console.log(err));
 };
