6b28deecc8f268638de063f77bafd81889e27f54
Tyler <tylermwatts@gmail.com>
2019-08-07 19:15:35 -0500

    untest

--- controllers/commandHandler.js
+++ controllers/commandHandler.js
@@ -6,7 +6,7 @@ dotenv.config();
 module.exports = message => {
   switch (message.content) {
     case '!test': {
-      return 'Test message.';
+      return 'Test message from my laptop.';
     }
     case '!help':
     case '!commands': {
